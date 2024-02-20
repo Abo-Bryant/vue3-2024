8b0524dd92af6d299df70f19b8aa2765a08e70f2
lb <484532503@qq.com>
2024-02-20 16:54:53 +0800

    登录模块表单校验

---
+++ src/utils/rules.ts
@@ -0,0 +1,13 @@
+import type { FieldRule } from 'vant'
+
+// 手机号校验规则
+const mobileRules = [
+  { required: true, message: '请输入手机号' },
+  { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确' }
+]
+// 密码校验规则
+const passwordRules: FieldRule[] = [
+  { required: true, message: '请输入密码' },
+  { pattern: /^\w{8,24}$/, message: '密码需8-24个字符' }
+]
+export { mobileRules, passwordRules }
