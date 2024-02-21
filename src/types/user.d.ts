816557532d979217527dd6583cdf2e61c4b5ecba
lb <484532503@qq.com>
2024-02-21 15:24:36 +0800

    个人中心-用户信息类型

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -18,3 +18,37 @@ export type CodeType =
   | 'changeMobile'
   | 'forgetPassword'
   | 'bindMobile'
+
+// 个人信息
+type OmitUser = Omit<User, 'token'>
+export type UserInfo = OmitUser & {
+  /** 关注 */
+  likeNumber: number
+  /** 收藏 */
+  collectionNumber: number
+  /** 积分 */
+  score: number
+  /** 优惠券 */
+  couponNumber: number
+  orderInfo: {
+    /** 待付款 */
+    paidNumber: number
+    /** 待发货 */
+    receivedNumber: number
+    /** 待收货 */
+    shippedNumber: number
+    /** 已完成 */
+    finishedNumber: number
+  }
+}
+
+// Omit Pick TS的内置类型
+// type Person = {
+//   name: string
+//   age: number
+//   gender: 0 | 1
+// }
+// // Omit 是从对象中排除一些属性,得到对象类型
+// type OmitPerson = Omit<Person, 'name'>
+// // Pick 是从对象中摘取一些属性,得到对象类型
+// type PickPerson = Pick<Person, 'name'>
