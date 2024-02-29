b11bbdd5e8defeda21a49bcde3da2e214c5d7fbe
lb <484532503@qq.com>
2024-02-29 11:12:38 +0800

    问诊室-默认消息

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,10 +2,10 @@ import router from '@/router'
 import { useUserStore } from '@/stores'
 import axios, { AxiosError, type Method } from 'axios'
 import { showToast } from 'vant'
-
+export const baseURL = 'https://consult-api.itheima.net/'
 const instance = axios.create({
   // 1. 基础地址，超时时间
-  baseURL: 'https://consult-api.itheima.net/',
+  baseURL,
   timeout: 10000
 })
 
--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -1,7 +1,41 @@
 <script setup lang="ts">
+import type { Socket } from 'socket.io-client'
 import RoomStatus from './components/RoommStatus.vue'
 import RoomAction from './components/RoomAction.vue'
 import RoomMessage from './components/RoomMessage.vue'
+import io from 'socket.io-client'
+import { onMounted, onUnmounted } from 'vue'
+import { baseURL } from '@/utils/request'
+import { useUserStore } from '@/stores'
+import { useRoute } from 'vue-router'
+const store = useUserStore()
+const route = useRoute()
+let socket: Socket
+onMounted(() => {
+  socket = io(baseURL, {
+    auth: {
+      token: `Bearer ${store.user?.token}`
+    },
+    query: {
+      orderId: route.query.orderId
+    }
+  })
+  socket.on('connect', () => {
+    console.log('连接成功')
+  })
+  socket.on('disconnect', () => {
+    console.log('连接关闭')
+  })
+  socket.on('error', () => {
+    console.log('发送错误')
+  })
+  socket.on('chatMsgList', (res) => {
+    console.log(res)
+  })
+})
+onUnmounted(() => {
+  socket.close()
+})
 </script>
 
 <template>
